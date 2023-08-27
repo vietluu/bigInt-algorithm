d188ef8c13878618e6f4ad538f0a4a26369c29a1
vietluu <74524082+vietluu@users.noreply.github.com>
2023-08-27 21:17:05 +0700

    add divided function

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function matchAction(e) {
       performanceTime(num1, num2, multiplyStrings);
       break;
     case "division":
-      performanceTime(num1, num2, multiplyStrings);
+      performanceTime(num1, num2, divideLargeNumbers);
       break;
     case "summation":
       performanceTime(num1, num2, multiplyStrings);
@@ -31,9 +31,9 @@ function multiplyStrings(num1, num2) {
 
   for (let i = bigInt1 - 1; i >= 0; i--) {
     for (let j = bigInt2 - 1; j >= 0; j--) {
-      const product = Number(num1[i]) * Number(num2[j]);
+      const mul_result = Number(num1[i]) * Number(num2[j]);
 
-      const temp = result[i + j + 1] + product;
+      const temp = result[i + j + 1] + mul_result;
       result[i + j + 1] = temp % 10;
       result[i + j] += Math.floor(temp / 10);
     }
@@ -46,11 +46,40 @@ function multiplyStrings(num1, num2) {
   return result.length === 0 ? "0" : result.join("");
 }
 
+function divideLargeNumbers(dividend, divisor) {
+  let quotient = '';
+  let remainder = '';
+
+  if(divisor == 0){
+    return 'không thể chia!'
+  }
+  if (dividend == 0) {
+    return  '0'
+  }
+
+  for (let i = 0; i < dividend.length; i++) {
+    let currentDigit = Number(remainder + dividend[i]);
+    
+    if (currentDigit < divisor) {
+      remainder = currentDigit.toString();
+      quotient += '0';
+    } else {
+      let tempQuotient = Math.floor(currentDigit / divisor);
+      let tempRemainder = currentDigit % divisor;
+      
+      remainder = tempRemainder.toString();
+      quotient += tempQuotient.toString();
+    }
+  }
+
+  return `${quotient.replace(/^0+/,'') || 0}` + ' dư: ' + `${remainder}`
+}
+
 // do thoi gian xu li
 function performanceTime(num1, num2, func) {
   const start = performance.now();
-  const result = func(num1, num2);
+  func(num1, num2);
   const end = performance.now();
   const time = end - start;
-  alert("result: " + result + " in: " + time.toFixed(3) + " milliseconds");
+  alert("result: " + func(num1, num2)  + " in: " + time.toFixed(3) + " milliseconds");
 }
