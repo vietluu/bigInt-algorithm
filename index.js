e1f1d28767609674f337074c19b2945294788101
viet <74524082+vietluu@users.noreply.github.com>
2023-09-25 17:12:38 +0700

    Feature/linked list (#2)
    * multiplyLargeNumbers with Negative numbers
    
    * add linked list

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+  matchAction(e);
+});
+
 const fileData = document.getElementById("data");
 fileData.addEventListener("change", (e) => {
   let reader = new FileReader();
@@ -16,13 +21,15 @@ fileData.addEventListener("change", (e) => {
 
 function matchAction(e) {
   e.preventDefault();
+  const list = new LinkedList();
+  const {multiplyLinkedList} = list;
   const num1 = document.getElementById("num1").value;
   const num2 = document.getElementById("num2").value;
   const select = document.getElementById("math").value;
   console.log(num1, num2, select);
   switch (select) {
     case "multiplication":
-      performanceTime(num1, num2, multiplyStrings);
+      performanceTime(num1, num2, multiplyLinkedList);
       break;
     case "Subtraction":
       performanceTime(num1, num2, multiplyStrings);
@@ -40,39 +47,41 @@ function matchAction(e) {
 }
 
 function multiplyStrings(num1, num2) {
-  
-
-  const isNegative = (num1.charAt(0) === '-' && num2.charAt(0) !== '-') || (num1.charAt(0) !== '-' && num2.charAt(0) === '-');
-  if (num1.charAt(0) === '-') {
+  let isNegative = false;
+  if (num1.charAt(0) === "-" && num2.charAt(0) !== "-") {
+    isNegative = true;
     num1 = num1.slice(1);
-  }
-  if (num2.charAt(0) === '-') {
+  } else if (num1.charAt(0) !== "-" && num2.charAt(0) === "-") {
+    isNegative = true;
     num2 = num2.slice(1);
   }
+
   const bigInt1 = num1.length;
   const bigInt2 = num2.length;
-  // tao mang  m+n  chu so 0
-  const result = Array(bigInt1 + bigInt2).fill(0);
+
+  const result = new Array(bigInt1 + bigInt2).fill(0);
 
   for (let i = bigInt1 - 1; i >= 0; i--) {
     for (let j = bigInt2 - 1; j >= 0; j--) {
       const mul_result = Number(num1[i]) * Number(num2[j]);
-
       const temp = result[i + j + 1] + mul_result;
       result[i + j + 1] = temp % 10;
       result[i + j] += Math.floor(temp / 10);
     }
   }
-  //loai bo so 0 thua 0 dau
-  while (result[0] === 0) {
-    result.shift();
+
+  let startIndex = 0;
+  while (result[startIndex] === 0 && startIndex < result.length - 1) {
+    startIndex++;
   }
-  if (isNegative && result.length > 0) {
-    return '-' + result.join("");
+
+  if (isNegative && result.length > startIndex) {
+    return "-" + result.slice(startIndex).join("");
   } else {
-    return result.length === 0 ? "0" : result.join("");
+    return result.length === startIndex
+      ? "0"
+      : result.slice(startIndex).join("");
   }
-  
 }
 
 function divideLargeNumbers(dividend, divisor) {
@@ -106,31 +115,43 @@ function divideLargeNumbers(dividend, divisor) {
 
 // do thoi gian xu li
 function performanceTime(num1, num2, func) {
+  const memoizeFn  = memoize(func);
   const start = performance.now();
-  func(num1, num2);
+ const result = memoizeFn(num1,num2);
   const end = performance.now();
   const time = end - start;
-  showModal(func(num1, num2),time.toFixed(3))
-  // alert(
-  //   "result: " + func(num1, num2) + " in: " + time.toFixed(3) + " milliseconds"
-  // );
+  showModal(result, time.toFixed(3));
 }
-function showModal(result,time){
-  const body = document.getElementById('body')
-  const div = document.createElement('div');
-  const btn = document.createElement('button')
-  btn.textContent = 'OK'
-  div.className += 'modal'
-  div.innerHTML +='<span>Result</span>'
-  div.innerHTML +=`<p>Kết quả của phép tính là ${result} với thời gian thực hiện trong ${time} milliseconds </p>`
-  div.appendChild(btn);
-  btn.onclick = ()=>{
-        body.removeChild(div)
-
+function memoize(fn) {
+  let cache = {};
+      return function(...args) {
+      const n = JSON.stringify(args);
+      if (n in cache) {
+        return cache[n];
+      }
+      else {
+        let result = fn(...args);
+        cache[n] = result;
+        return result;
+      
+    }
+      }
   }
+function showModal(result, time) {
+  const body = document.getElementById("body");
+  const div = document.createElement("div");
+  const btn = document.createElement("button");
+  btn.textContent = "OK";
+  div.className += "modal";
+  div.innerHTML += "<span>Result</span>";
+  div.innerHTML += `<p>Kết quả của phép tính là ${result} với thời gian thực hiện trong ${time} milliseconds </p>`;
+  div.appendChild(btn);
+  btn.onclick = () => {
+    clearTimeout(timeout);
+    body.removeChild(div);
+  };
   body.appendChild(div);
-  setTimeout(()=>{
-    body.removeChild(div)
-  },3000)
-  
+  const timeout = setTimeout(() => {
+    body.removeChild(div);
+  }, 3000);
 }
